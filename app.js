2af4df0279d875ee98274d54224f834cb1871f23
samrm111 <samuelrondeaumillaire@gmail.com>
2016-02-15 00:36:40 -0500

    Implement js object with BSON types to string parsing

--- app.js
+++ app.js
@@ -1,17 +1,67 @@
 var mongojs = require('mongojs');
+var mongodb = require('mongodb');
 var safeEval = require('safe-eval');
 
-module.exports = function () {
-
-
+var StringToBSON = function() {
 
     /**
      * Parse a string to a javascript object with BSON types
-     * @param  {[type]} string [description]
-     * @return {[type]}        [description]
+     * @param  {String} string The string to parse
+     * @return {Object}        The result of the parsing
      */
     this.parse = function(string) {
         return safeEval(string, context);
+    };
+
+    /**
+     * Convert an object containing BSON values to a formatted string
+     * @param  {Object} obj       The object to parse
+     * @param  {Boolean} formatted If set to true, new line at each objects
+     * @return {String}           The result of the parsing
+     */
+    this.toString = function(obj, formatted) {
+        formatted = typeof formatted == 'undefined' ? false : formatted;
+        var newline = formatted ? '\n' : '';
+        //create an array that will later be joined into a string.
+        var string = [];
+
+        //is object
+        if (obj == undefined) {
+            return String(obj);
+        } else if (typeof(obj) == "object" && (obj.join == undefined)) {
+            for (prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    if (obj[prop] instanceof mongodb.ObjectID) {
+                        string.push(newline + prop + ': ' + 'ObjectID("' + obj[prop].toString() + "\")");
+                    } else if (obj[prop] instanceof Date) {
+                        string.push(newline + prop + ': ' + 'ISOString("' + obj[prop].toString() + "\")");
+                    } else if (obj[prop] instanceof mongojs.Long) {
+                        string.push(newline + prop + ': ' + 'NumberLong("' + obj[prop].toString() + "\")");
+                    } else {
+                        string.push(newline + prop + ': ' + this.toString(obj[prop]));
+                    }
+
+                }
+            };
+            return "{" + string.join(",") + newline + "}";
+
+            //is array
+        } else if (typeof(obj) == "object" && !(obj.join == undefined)) {
+            for (prop in obj) {
+                string.push(this.toString(obj[prop]));
+            }
+            return "[" + string.join(",") + "]";
+
+            //is function
+        } else if (typeof(obj) == "function") {
+            string.push(newline + obj.toString())
+
+            //all other values can be done with JSON.stringify
+        } else {
+            string.push(JSON.stringify(obj))
+        }
+
+        return string.join(",");
     };
 
     /**
@@ -19,21 +69,22 @@ module.exports = function () {
      * @type {Object}
      */
     var context = {
-        ObjectID : function(id) {
+        ObjectID: function(id) {
             try {
                 return new mongojs.ObjectID(id);
-            } catch(err) {
+            } catch (err) {
                 throw 'Invalid ObjectID: ' + id;
             }
         },
-        ISODate : function(ISOString) {
+        ISODate: function(ISOString) {
             return new Date(ISOString);
         },
-        NumberLong : function(number) {
+        NumberLong: function(number) {
             return mongojs.Long.fromString(number);
         },
-        Date : function(dateString) {
+        Date: function(dateString) {
             return new Date(dateString);
         }
     };
 }
+module.exports = new StringToBSON();
